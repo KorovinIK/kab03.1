7ea4b3de7921142254f05f56847bedd82602aa46
Shrimp2 (KorovinIK) <korovinik@mpei.ru>
2022-05-30 11:19:09 +0300

    histogram: min max

---
+++ histogram.h
@@ -0,0 +1,13 @@
+#ifndef HISTOGRAM_H_INCLUDED
+#define HISTOGRAM_H_INCLUDED
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void find_minmax(const vector<double>& numbers, double& min, double& max);
+
+
+
+#endif // HISTOGRAM_H_INCLUDED
