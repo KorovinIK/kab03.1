bd24362cafd39c7b88e2ec33c68779e6c2519626
Shrimp2 (KorovinIK) <korovinik@mpei.ru>
2022-05-30 12:21:51 +0300

    code: histog

--- histogram.cpp
+++ histogram.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 void find_minmax(const vector<double>& numbers, double& min, double& max) {
+    if(numbers.size() != 0)
+    {
+        //return;
+
     min = numbers[0];
     max = numbers[0];
     for (double number : numbers){
@@ -10,5 +14,6 @@ void find_minmax(const vector<double>& numbers, double& min, double& max) {
             min = number;}
         if (max < number){
             max = number;}
+    }
     }
 }
