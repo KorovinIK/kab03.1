34b36cd9e52ca5999f15add2a0c4e1981e9be556
Shrimp2 (KorovinIK) <korovinik@mpei.ru>
2022-05-30 13:24:52 +0300

    test: p 31

--- test.cpp
+++ test.cpp
@@ -10,8 +10,33 @@ test_positive() {
     assert(min == 1);
     assert(max == 3);
 }
+void test_negative()
+{
+double min = 0; double max = 0;
+find_minmax({-2, -1, -3}, min, max);
+assert(min == -3);
+assert(max == -1);
+}
+
+void test_equal()
+{
+double min = 0; double max = 0;
+find_minmax({3, 3, 3}, min, max);
+assert(min == 3);
+assert(max == 3);
+}
+
+void test_one_num()
+{
+double min = 0; double max = 0;
+find_minmax({9}, min, max); assert(min == 9);
+assert(max == 9);
+}
 
 int
 main() {
     test_positive();
+    test_negative();
+    test_equal();
+    test_one_num();
 }
