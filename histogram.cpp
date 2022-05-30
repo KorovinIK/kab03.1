9462c81dccc6193f153d8b897f5ff29247387cbe
Shrimp2 (KorovinIK) <korovinik@mpei.ru>
2022-05-30 11:20:45 +0300

    histogram: source create

---
+++ histogram.cpp
@@ -0,0 +1,14 @@
+#include "histogram.h"
+
+using namespace std;
+
+void find_minmax(const vector<double>& numbers, double& min, double& max) {
+    min = numbers[0];
+    max = numbers[0];
+    for (double number : numbers){
+        if (min > number){
+            min = number;}
+        if (max < number){
+            max = number;}
+    }
+}
